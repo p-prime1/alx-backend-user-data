9c8e8751624724697460dd99bbfb205632c8aaaf
p-prime1 <preciousnwaka95@gmail.com>
2024-11-09 22:50:46 +0100

    documented the modules

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Module contains a filter datum that returns an obfuscated message"""
 from typing import List
-import re, logging
+import re
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -29,14 +30,19 @@ class RedactingFormatter(logging.Formatter):
     """
 
     REDACTION: str = "***"
-    FORMAT: str = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    FORMAT: str = """[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s:
+            %(message)s"""
     SEPERATOR: str = ";"
 
     def __init__(self, fields: List[str]):
+        """Initialzes Formatter from the parent class"""
         self.fields = fields
         super(RedactingFormatter, self).__init__(self.FORMAT)
 
     def format(self, record: logging.LogRecord) -> str:
+        """Method obfuscate (hides) sensitive information and format
+        using the RedactingFormatter"""
         original_message = super().format(record)
-        obfuscated_message = filter_datum(self.fields, self.REDACTION, original_message, self.SEPERATOR)
+        obfuscated_message = filter_datum(self.fields, self.REDACTION,
+                                          original_message, self.SEPERATOR)
         return obfuscated_message
