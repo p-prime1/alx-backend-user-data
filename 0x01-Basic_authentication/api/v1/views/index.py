339ec5b0edc9781d63ffbd49312dfbeb590e82bb
p-prime1 <preciousnwaka95@gmail.com>
2024-11-18 23:52:13 +0100

    added a handler for staus code unauthorized 401

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -14,6 +14,12 @@ def status() -> str:
     return jsonify({"status": "OK"})
 
 
+@app_views.route('/api/v1/unauthorized', methods['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """Get /api/v1/unauthorized"""
+    abort(401)
+
+
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
