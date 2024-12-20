de05fcc2826ba65c7490f317bc143184995d96f9
p-prime1 <preciousnwaka95@gmail.com>
2024-12-20 15:44:20 +0100

    pushing again

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,39 +4,28 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request, Response
+from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix='/api/v1')
+app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.errorhandler(404)
-def not_found(error) -> tuple:
+def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> tuple:
-    """
-        Unauthorized access
-        Return:
-            Unauthorized
+def unauthorized(error) -> str:
+    """Unauthoreized error handler
     """
     return jsonify({"error": "Unauthorized"}), 401
-
-
-@app.errorhandler(403)
-def forbidden(error) -> tuple:
-    """
-    Forbidden handelr
-    """
-    return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
-""" Module of Index views, including routes for status,
-    unauthorized, and forbidden access.
+""" Module of Index views
 """
-from flask import jsonify, abort, Response
+from flask import jsonify, abort
 from api.v1.views import app_views
-from typing import Any
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status() -> Response:
+def status() -> str:
     """ GET /api/v1/status
     Return:
       - the status of the API
@@ -16,27 +14,17 @@ def status() -> Response:
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/app/v1/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized() -> Any:
-    """ GET /api/v1/unauthorized
-    Raises:
-        HTTPException: A 401 Unauthorized error using Flask's `abort`
-        function
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized_handler():
+    """GET /api/v1/status
+    Raise:
+        an abort call
     """
     abort(401)
 
 
-@app_views.route('/api/v1/forbidden', mehtods=['GET'], strict_slashes=False)
-def forbidden() -> Any:
-    """ Get /api/v1/forbidden
-    Raises
-        forbidden: A flask abort call with a 403 status code
-    """
-    abort(403)
-
-
 @app_views.route('/stats/', strict_slashes=False)
-def stats() -> Response:
+def stats() -> str:
     """ GET /api/v1/stats
     Return:
       - the number of each objects
